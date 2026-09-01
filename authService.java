7b27e7c3b6c22e283de1aef026f939a3bea9130e
Jeethan Shan Tauro <148562113+JeethanTauro@users.noreply.github.com>
2026-09-01 14:16:50 +0530

    add isSecure function to authService.java

--- authService.java
+++ authService.java
@@ -5,8 +5,12 @@ class authService{
   public static int generateToken(){
     //generate tokem
   }
+  public static boolean isSecure(){
+    //is secure check
+  }
   public static void main(String[] args){
     //auth service code
   }
+  
 
 }
